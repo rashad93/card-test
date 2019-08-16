89a0b106cce9735a54d473671adf6bad57bebb1e
Rashad <34395361+rashad93@users.noreply.github.com>
2019-08-17 01:19:16 +0800

    Add files via upload

---
+++ precache-manifest.ffbdad5eb0dd14404ee119182c4e4fa9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb9fdee135ce58bccd1e1d89770c6459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5f4d8eda5b3ca9b9bdb",
+    "url": "/static/css/main.9b55cc0f.chunk.css"
+  },
+  {
+    "revision": "3df22c9655cbbced03db",
+    "url": "/static/js/2.3dda2a06.chunk.js"
+  },
+  {
+    "revision": "c5f4d8eda5b3ca9b9bdb",
+    "url": "/static/js/main.196571a5.chunk.js"
+  },
+  {
+    "revision": "698b779985fadf53a50a",
+    "url": "/static/js/runtime~main.7c422b21.js"
+  }
+]);
